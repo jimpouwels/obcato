7a466cd915459cc73478fdad1aa33ba9a7042745
jimpouwels <jim.pouwels@gmail.com>
2013-06-06 20:28:39 +0200

    Refactor: backend class

--- cms/www_root/admin/backend.php
+++ cms/www_root/admin/backend.php
@@ -17,36 +17,20 @@
 		private $_pre_handlers;
 		private $_settings;
 		private $_current_module;
+		private $_module_visual;
 	
 		public function __construct($identifier) {
 			$this->_identifier = $identifier;
 			$this->_session = new Session();
 			$this->_settings = Settings::find();
-			
-			$this->_pre_handlers = array();
-			$this->_pre_handlers[] = new LinkPreHandler();
-			$this->_pre_handlers[] = new ElementPreHandler();
-			$this->_pre_handlers[] = new ModulePreHandler($this);
+			$this->initializePreHandlers();
 		}
 		
 		public function start() {
-			$module_visual = null;;
-		
 			$this->isAuthenticated();
 			$this->runPreHandlers();
-			if (!is_null($this->_current_module)) {
-				require_once "modules/" . $this->_current_module->getIdentifier() . "/activator.php";
-				$class = $this->_current_module->getClass();
-				$module_visual = new $class($this->_current_module);
-				$module_visual->preHandle();
-			}
-			if ($this->isPopupView()) {
-				$popup = new Popup($_GET['popup']);
-				$popup->render();
-			} else {
-				$cms = new Cms($module_visual, $this->_settings->getWebsiteTitle());
-				$cms->render();
-			}
+			$this->runModulePreHandler();
+			$this->renderCms();
 		}
 		
 		public function isAuthenticated() {
@@ -61,19 +45,51 @@
 		*/
 		public function setCurrentModule($current_module) {
 			$this->_current_module = $current_module;
+			require_once "modules/" . $this->_current_module->getIdentifier() . "/activator.php";
+			$class = $this->_current_module->getClass();
+			$this->_module_visual = new $class($this->_current_module);
+		}
+		
+		private function renderCms() {
+			if ($this->isPopupView())
+				$this->renderPopupView();
+			else
+				$this->renderCmsView();
+		}
+		
+		private function renderCmsView() {
+			$cms = new Cms($this->_module_visual, $this->_settings->getWebsiteTitle());
+			$cms->render();
+		}
+
+		private function renderPopupView() {
+			$popup = new Popup($_GET['popup']);
+			$popup->render();
+		}
+		
+		private function runModulePreHandler() {
+			if (!is_null($this->_module_visual)) {
+				$this->_module_visual->preHandle();
+			}
+		}
+		
+		private function initializePreHandlers() {
+			$this->_pre_handlers = array();
+			$this->_pre_handlers[] = new LinkPreHandler();
+			$this->_pre_handlers[] = new ElementPreHandler();
+			$this->_pre_handlers[] = new ModulePreHandler($this);
 		}
 		
 		private function runPreHandlers() {
 			foreach ($this->_pre_handlers as $pre_handler) {
-				if ($pre_handler instanceof PreHandler) {
+				if ($pre_handler instanceof PreHandler)
 					$pre_handler->handle();
-				}
 			}
 		}
 		
 		private function redirectToLoginPage() {
 			session_destroy();
-			$org_url = NULL;
+			$org_url = null;
 			if ($_SERVER['REQUEST_URI'] != '/admin/') {
 				$org_url = '?org_url=' . urlencode($_SERVER['REQUEST_URI']);
 			}
