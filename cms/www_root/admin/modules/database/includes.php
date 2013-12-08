4d03952e332c22265a9a3e8f7ef62fad1ddc1ded
jimpouwels <jim.pouwels@gmail.com>
2013-12-08 10:26:20 +0100

    removed obsolete files

--- cms/www_root/admin/modules/database/includes.php
+++
@@ -1,6 +0,0 @@
-<?php
-	// No direct access
-	defined('_ACCESS') or die;
-?>
-
-<link rel="stylesheet" href="/admin/static.php?static=/modules/<?= $current_module->getIdentifier(); ?>/css/database.css" type="text/css" />
