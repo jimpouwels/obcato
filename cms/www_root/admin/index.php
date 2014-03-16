ad3936992b447a55524e9034ce47b7409a0f2b02
jimpouwels <jim.pouwels@gmail.com>
2014-03-16 09:44:19 +0100

    removed obsolete code from index page

--- cms/www_root/admin/index.php
+++ cms/www_root/admin/index.php
@@ -12,25 +12,5 @@
 	
 	$backend = new Backend("site_administrator");
 	$backend->start();
-	
-	// AUTHENTICATE
-	//include_once FRONTEND_REQUEST . "core/data/session.php";
-	//Session::isAuthenticated();
-	
-	// LOAD THE CURRENT MODULE
-	//include_once FRONTEND_REQUEST . "pre_handlers/module_handler.php";
-	
-	// LAUNCH CURRENT MODULE PRE-HANDLER
-	//if (isset($current_module) && !is_null($current_module)) {
-	//	if (!is_null($current_module->getPreHandler()) && $current_module->getPreHandler() != '') {
-	//		include_once FRONTEND_REQUEST . "modules/" . $current_module->getIdentifier() . "/" . $current_module->getPreHandler();
-	//	}
-	//}
-	
-	// INCLUDE SYSTEM PRE-HANDLERS
-	//include_once FRONTEND_REQUEST . "pre_handlers/element_handler.php";
-	//include_once FRONTEND_REQUEST . "pre_handlers/link_handler.php";
 
-	// START HTML RENDERING
-	//include_once FRONTEND_REQUEST . "html/basic_parts/index.php";
 ?>
