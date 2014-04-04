7952dd8711ffe1525cfcdb2742d9daf05176d447
jimpouwels <jim.pouwels@gmail.com>
2014-04-04 22:02:41 +0200

    Refactor: Removed comments and replaced quotes

--- cms/www_root/admin/frontend/handlers/request_handler.php
+++ cms/www_root/admin/frontend/handlers/request_handler.php
@@ -1,7 +1,7 @@
 <?php
 
 	// No direct access
-	defined('_ACCESS') or die;
+	defined("_ACCESS") or die;
 	
 	require_once FRONTEND_REQUEST . "core/data/settings.php";
 	require_once FRONTEND_REQUEST . "database/dao/page_dao.php";
@@ -10,9 +10,6 @@
 		
 		private $_page_dao;
 	
-		/*
-			Handles the incoming frontend request.
-		*/
 		public function handleRequest() {
 			$this->_page_dao = PageDao::getInstance();
 			$page = $this->getPageFromRequest();
@@ -20,28 +17,19 @@
 			$this->renderPage($page, $article);
 		}
 		
-		/*
-			Renders the page.
-			
-			@param $page The page to render
-			@param $article The article to render
-		*/
 		private function renderPage($page, $article) {
 			$website_settings = Settings::find();
 					
 			define("TEMPLATE_DIR", $website_settings->getFrontendTemplateDir());
 			
 			if (!is_null($page)) {
-				include TEMPLATE_DIR . '/' . $page->getTemplate()->getFileName();
+				include TEMPLATE_DIR . "/" . $page->getTemplate()->getFileName();
 			}
 		}
 		
-		/*
-			Returns the requested page.
-		*/
 		private function getPageFromRequest() {					
-			if (isset($_GET['id']) && $_GET['id'] != '') {
-				$page_id = $_GET['id'];
+			if (isset($_GET["id"]) && $_GET["id"] != "") {
+				$page_id = $_GET["id"];
 				$page = $this->_page_dao->getPage($page_id);
 			} else {
 				$settings_dao = SettingsDao::getInstance();
@@ -50,17 +38,14 @@
 			return $page;
 		}
 		
-		/*
-			Returns the requested article.
-		*/
 		private function getArticleFromRequest() {
 			include_once FRONTEND_REQUEST . "database/dao/article_dao.php";
 			
 			$article_dao = ArticleDao::getInstance();
 			
 			$article = NULL;
-			if (isset($_GET['articleid']) && $_GET['articleid'] != '') {
-				$article = $article_dao->getArticle($_GET['articleid']);
+			if (isset($_GET["articleid"]) && $_GET["articleid"] != "") {
+				$article = $article_dao->getArticle($_GET["articleid"]);
 			}
 			
 			return $article;
