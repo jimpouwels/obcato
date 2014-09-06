bb60ebd959ab6fe6d923a643447d606ca5b33054
jimpouwels <jim.pouwels@gmail.com>
2014-09-06 07:34:09 +0200

    Fixed incorrect javascript update logic

--- cms/www_root/admin/static/js/install.js
+++ cms/www_root/admin/static/js/install.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
     });
 
     $('.delete_install_files_button').click(function() {
-        $('#installation_finish_type').value = "delete_install_files";
+        $('#installation_finish_type').val("delete_install_files");
         $('#install_form').submit();
     });
 
     $('.redirect_to_login').click(function() {
-        $('#installation_finish_type').value = "redirect_to_login";
+        $('#installation_finish_type').val("redirect_to_login");
         $('#install_form').submit();
     });
 });
