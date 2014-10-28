29a1c082cbe671903c2078c10cef23b268f97263
Jim Pouwels <jim.pouwels@gmail.com>
2014-10-28 16:52:16 +0100

    SA0013: Check whether the uploaded file is a valid ZIP archive

--- cms/admin/modules/components/install_request_handler.php
+++ cms/admin/modules/components/install_request_handler.php
@@ -19,12 +19,28 @@
             $form = new InstallComponentForm();
             try {
                 $form->loadFields();
+                $this->handleComponentZip($form->getFilePath());
             } catch (FormException $e) {
                 Notifications::setFailedMessage('U dient een component archief te kiezen');
+            } catch (InvalidComponentArchiveException $e) {
+                Notifications::setFailedMessage('Dit is geen geldig ZIP archief');
             }
+        }
+
+        private function handleComponentZip($file_path) {
+            $resource = zip_open($file_path);
+            if (is_numeric($resource)) throw new InvalidComponentArchiveException("Invalid ZIP archive");
         }
 
         private function isInstallComponentAction() {
             return isset($_POST['action']) && $_POST['action'] == 'install_component';
         }
+    }
+
+    class InvalidComponentArchiveException extends Exception {
+
+        public function __construct($message = "") {
+            parent::__construct($message);
+        }
+
     }
