903c1f36988171423ade28c79eca3f422fa03407
Jim Pouwels <jim.pouwels@gmail.com>
2014-11-14 20:51:02 +0100

    REFACTOR: Cleaned up LinkDao code

--- public_root/admin/database/dao/link_dao.php
+++ public_root/admin/database/dao/link_dao.php
@@ -7,133 +7,81 @@
     include_once CMS_ROOT . "core/data/link.php";
 
     class LinkDao {
-    
-        // singleton
+
         private static $instance;
-        
-        /*
-            Private constructor.
-        */
+        private $_mysql_connector;
+
         private function __construct() {
+            $this->_mysql_connector = MysqlConnector::getInstance();
         }
-        
-        /*
-            Creates and returns a new instance of the DAO
-            if it not yet exists.
-        */
+
         public static function getInstance() {
             if (!self::$instance) {
                 self::$instance = new LinkDao();
             }
             return self::$instance;
         }
-        
-        /*
-            Creates and persists a new link.
-            
-            @param $element_holder_id The element holder to add the link to
-        */
+
         public function createLink($element_holder_id) {
             $new_link = new Link();
             $new_link->setTitle('Nieuwe link');
             $new_link->setCode($new_link->getId());
             $new_link->setParentElementHolderId($element_holder_id);
             $new_link->setType(Link::INTERNAL);
-            
-            $new_id = $this->persistLink($new_link);
-            $new_link->setId($new_id);
-            
+            $this->persistLink($new_link);
             return $new_link;
         }
-        
-        /*
-            Persists the given link.
-            
-            @param $new_link The page to persist
-        */
+
         public function persistLink($new_link) {
-            $mysql_database = MysqlConnector::getInstance(); 
-            
             $query = "INSERT INTO links (title, target_address, type, code, target, target_element_holder, parent_element_holder)
                       VALUES ('" . $new_link->getTitle() . "', NULL, '" . $new_link->getType() . 
                       "', '" . $new_link->getCode() . "', '_self', NULL, " . $new_link->getParentElementHolderId() . ")";
 
-            $mysql_database->executeQuery($query);
-            
-            return $mysql_database->getInsertId();
+            $this->_mysql_connector->executeQuery($query);
+            $new_link->setId($this->_mysql_connector->getInsertId());
         }
-        
-        /*
-            Returns all links for the given element holder.
-            
-            @param $element_holder_id The element holder ID to find the links for
-        */
+
         public function getLinksForElementHolder($element_holder_id) {
-            $mysql_database = MysqlConnector::getInstance(); 
-            
             $query = "SELECT * FROM links WHERE parent_element_holder = " . $element_holder_id;
-            $result = $mysql_database->executeQuery($query);
+            $result = $this->_mysql_connector->executeQuery($query);
             $links = array();
             while ($row = $result->fetch_assoc()) {
                 $link = Link::constructFromRecord($row);
-                
                 $links[] = $link;
             }
             return $links;
         }
-        
-        /*
-            Deletes the given link.
-            
-            @param $link The link to delete
-        */
+
         public function deleteLink($link) {
-            $mysql_database = MysqlConnector::getInstance(); 
-            
-            $query = "DELETE FROM links WHERE id = " . $link->getId();
-            
-            $mysql_database->executeQuery($query);
+            $statement = $this->_mysql_connector->prepareStatement('DELETE FROM links WHERE id = ?');
+            $link_id = $link->getId();
+            $statement->bind_param('i', $link_id);
+            $this->_mysql_connector->executeStatement($statement);
         }
-        
-        /*
-            Updates the given link object.
-            
-            @param @link The link to update
-        */
+
         public function updateLink($link) {
-            $mysql_database = MysqlConnector::getInstance(); 
-                        
-            if (!is_null($link->getTargetElementHolder()) && $link->getTargetElementHolder() != '') {
+            if (!is_null($link->getTargetElementHolder()) && $link->getTargetElementHolder() != '')
                 $link->setType(Link::INTERNAL);
-            } else {
+            else
                 $link->setType(Link::EXTERNAL);
-            }
             
             $query = "UPDATE links SET title = '" . $link->getTitle() . "', target_address = '" . $link->getTargetAddress() . "',
                       code = '" . $link->getCode() . "', target = '" . $link->getTarget() . "', type = '" . $link->getType() . "'";
             
-            if ($link->getTargetElementHolderId() != '' && !is_null($link->getTargetElementHolderId())) {
+            if ($link->getTargetElementHolderId() != '' && !is_null($link->getTargetElementHolderId()))
                 $query = $query . ", target_element_holder = " . $link->getTargetElementHolderId();
-            } else {
+            else
                 $query = $query . ", target_element_holder = NULL";
-            }
             $query = $query . " WHERE id = " . $link->getId();
-            
-            $mysql_database->executeQuery($query);
+            $this->_mysql_connector->executeQuery($query);
         }
-        
-        /*
-            Returns a list of links withouth a target.
-        */
+
         public function getBrokenLinks() {
-            $mysql_database = MysqlConnector::getInstance(); 
-            
             $query = "SELECT * FROM links WHERE target_address IS NULL AND target_element_holder IS NULL";
-            $result = $mysql_database->executeQuery($query);
+            $result = $this->_mysql_connector->executeQuery($query);
             $links = array();
             while ($row = $result->fetch_assoc()) {
                 $link = Link::constructFromRecord($row);
-                
                 $links[] = $link;
             }
             return $links;            
