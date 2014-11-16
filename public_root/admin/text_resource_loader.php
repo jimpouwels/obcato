ce70f9db687919dadb2ab8a9effe6ea54763517b
Jim Pouwels <jim.pouwels@gmail.com>
2014-11-16 09:43:53 +0100

    SA0018: Skip comments when loading text resources

--- public_root/admin/text_resource_loader.php
+++ public_root/admin/text_resource_loader.php
@@ -2,6 +2,7 @@
     defined('_ACCESS') or die;
 
     require_once CMS_ROOT . "authentication/session.php";
+    require_once CMS_ROOT . "utilities/string_utility.php";
 
     class TextResourceLoader {
 
@@ -10,10 +11,17 @@
             $language = Session::getCurrentLanguage();
             $file = fopen(STATIC_DIR . '/text_resources/' . $language . '.txt', 'rb');
             while (!feof($file)) {
-                $parts = explode(':', fgets($file));
-                $resources[trim($parts[0])] = trim($parts[1]);
+                $line = fgets($file);
+                if (!$this->isComment($line)) {
+                    $parts = explode(':', $line);
+                    $resources[trim($parts[0])] = trim($parts[1]);
+                }
             }
             return $resources;
+        }
+
+        private function isComment($line) {
+            return StringUtility::startsWith(trim($line), '#');
         }
 
     }
