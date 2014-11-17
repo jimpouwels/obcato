6844ab37a57eb3837e313600c7aa1fb3e64b17ec
Jim Pouwels <jim.pouwels@gmail.com>
2014-11-17 12:54:56 +0100

    Fixed: Incorrectly refactored password field

--- public_root/admin/view/views/form_password_field.php
+++ public_root/admin/view/views/form_password_field.php
@@ -9,7 +9,7 @@
         private $_template_engine;
 
         public function __construct($name, $label, $value, $mandatory, $class_name) {
-            parent::__construct($name, $value, $label, $mandatory, false, $class_name);
+            parent::__construct($name, $label, $value, $mandatory, false, $class_name);
             $this->_template_engine = TemplateEngine::getInstance();
         }
     
