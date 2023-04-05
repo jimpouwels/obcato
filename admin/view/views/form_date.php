e5e8f90e898364682bea3d82e5b1d11e8a358d1b
Jim Pouwels <jim.pouwels@philips.com>
2023-04-05 16:37:06 +0200

    Fixed datefield

--- admin/view/views/form_date.php
+++ admin/view/views/form_date.php
@@ -3,14 +3,14 @@
     
     require_once CMS_ROOT . "view/views/form_textfield.php";
     
-    class DateField extends TextField {
+    class DateField extends FormField {
     
         private static $TEMPLATE = "system/form_date.tpl";
     
         private $_template_engine;
     
         public function __construct($name, $label, $value, $mandatory, $class_name) {
-            parent::__construct($name, $label, $value, $mandatory, false, $class_name);
+            parent::__construct($name, $value, $label, $mandatory, false, $class_name);
             $this->_template_engine = TemplateEngine::getInstance();
         }
     
