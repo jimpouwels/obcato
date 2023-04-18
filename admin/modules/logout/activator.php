ce8a405b2e6c84b4a1c5c5e5f2c4ff218d5f9893
Jim Pouwels <jim.pouwels@philips.com>
2023-04-18 14:51:32 +0200

    SA0060: Make the code type safe

--- admin/modules/logout/activator.php
+++ admin/modules/logout/activator.php
@@ -4,32 +4,34 @@
     defined('_ACCESS') or die;
     
     require_once CMS_ROOT . "view/views/module_visual.php";
-    require_once CMS_ROOT . "modules/logout/logout_pre_handler.php";
+    require_once CMS_ROOT . "modules/logout/logout_request_handler.php";
 
     class LogoutModuleVisual extends ModuleVisual {
     
-        private $_module;
-        private $_logout_pre_handler;
+        private Module $_module;
+        private LogoutRequestHandler $_logout_request_handler;
     
         public function __construct($module) {
             parent::__construct($module);
             $this->_module = $module;
-            $this->_logout_pre_handler = new LogoutPreHandler();
+            $this->_logout_request_handler = new LogoutRequestHandler();
         }
     
         public function render(): string {
             return "";
         }
     
-        public function getActionButtons() {
+        public function getActionButtons(): array {
+            return array();
         }
         
-        public function renderHeadIncludes() {
+        public function renderHeadIncludes(): string {
+            return "";
         }
         
-        public function getRequestHandlers() {
+        public function getRequestHandlers(): array {
             $request_handlers = array();
-            $request_handlers[] = $this->_logout_pre_handler;
+            $request_handlers[] = $this->_logout_request_handler;
             return $request_handlers;
         }
         
--- admin/modules/logout/logout_request_handler.php
+++ admin/modules/logout/logout_request_handler.php
@@ -3,13 +3,13 @@
     require_once CMS_ROOT . "authentication/authenticator.php";
     require_once CMS_ROOT . "request_handlers/http_request_handler.php";
     
-    class LogoutPreHandler extends HttpRequestHandler {
+    class LogoutRequestHandler extends HttpRequestHandler {
     
-        public function handleGet() {
-            Authenticator::logOut($_SESSION['username']);
+        public function handleGet(): void {
+            Authenticator::logOut();
         }
         
-        public function handlePost() {
+        public function handlePost(): void {
         }
         
     }
