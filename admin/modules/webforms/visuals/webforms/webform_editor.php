589ab310e8fe55f80e58dbbf95c8ebc88287bff0
Jim Pouwels <jim.pouwels@philips.com>
2023-05-02 16:58:35 +0200

    SA0067: title edit field for webform

--- admin/modules/webforms/visuals/webforms/webform_editor.php
+++ admin/modules/webforms/visuals/webforms/webform_editor.php
@@ -2,11 +2,15 @@
     defined('_ACCESS') or die;
 
     require_once CMS_ROOT . "view/views/panel.php";
+    require_once CMS_ROOT . "view/views/form_textfield.php";
 
     class WebFormEditor extends Panel {
 
+        private WebForm $_current_webform;
+
         public function __construct(?WebForm $current_webform) {
             parent::__construct("webforms_editor_panel_title");
+            $this->_current_webform = $current_webform;
         }
 
         public function getPanelContentTemplate(): string {
@@ -14,7 +18,10 @@
         }
 
         public function loadPanelContent(Smarty_Internal_Data $data): void {
-
+            $title_text_field = new TextField("title", "webforms_editor_title_field", $this->_current_webform->getTitle(), true, false, null);
+            $data->assign("action_form_id", ACTION_FORM_ID);
+            $data->assign("id", $this->_current_webform->getId());
+            $data->assign("title_field", $title_text_field->render());
         }
 
     }
