b8dae0aada89418901cde6a759a91789f59488c4
Jim Pouwels <jim.pouwels@philips.com>
2023-05-14 18:09:32 +0200

    Fixed serving uploads

--- admin/upload.php
+++ admin/upload.php
@@ -3,6 +3,8 @@
     define("_ACCESS", "GRANTED");
     define("CMS_ROOT", '');
 
+    require_once CMS_ROOT . "view/views/visual.php";
+    require_once CMS_ROOT . "view/views/panel.php";
     require_once CMS_ROOT . "authentication/authenticator.php";
     require_once CMS_ROOT . "database_config.php";
     require_once CMS_ROOT . "constants.php";
