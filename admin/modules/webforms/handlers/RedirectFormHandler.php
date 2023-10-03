b1cb1ff61ef392449c60dfdfcc5a9028b9b718d4
Jim Pouwels <jim.pouwels@philips.com>
2023-10-03 10:27:10 +0200

    Refactor: Renaming files

--- admin/modules/webforms/handlers/RedirectFormHandler.php
+++ admin/modules/webforms/handlers/RedirectFormHandler.php
@@ -17,7 +17,7 @@ class RedirectFormHandler extends Formhandler {
     }
 
     public function getRequiredProperties(): array {
-        require_once CMS_ROOT . '/modules/webforms/visuals/redirect_form_handler_editor.php';
+        require_once CMS_ROOT . '/modules/webforms/visuals/RedirectFormHandlerEditor.php';
         return array(
             new HandlerProperty('page_id', 'textfield', new RedirectFormHandlerEditor()),
         );
