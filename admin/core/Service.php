b87fa92ca8f06137fb5f42e7c56ad28111bdd9a8
Jim Pouwels <jim.pouwels@philips.com>
2023-10-04 16:46:11 +0200

    Removed class

--- admin/core/Service.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('_ACCESS') or die;
-
-interface Service {
-    protected function getTextResource(string $identifier): string {
-        return Session::getTextResource($identifier);
-    }
-}
