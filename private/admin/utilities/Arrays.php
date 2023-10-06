c9d3d20ed6d80b29e841ca8170f673e84a6cf9b0
Jim Pouwels <jim.pouwels@philips.com>
2023-10-06 14:56:55 +0200

    Fixed error in arrays

--- private/admin/utilities/Arrays.php
+++ private/admin/utilities/Arrays.php
@@ -2,7 +2,7 @@
 
 class Arrays {
 
-    static function firstMatch(array $array, Closure $lambda): ?object {
+    static function firstMatch(array $array, Closure $lambda): object|array|string|null {
         foreach ($array as $val) {
             if ($lambda($val)) {
                 return $val;
