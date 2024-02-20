7d4d7a665d8bdd3af6319ab1f5deb038d7e8e2c1
Jim Pouwels <jim.pouwels@philips.com>
2024-02-20 10:59:57 +0100

    move

--- src/private/admin/database/dao/ConfigDao.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Obcato\Core;
-
-interface ConfigDao {
-    public function updateCaptchaSecret(string $captchaSecret): void;
-
-    public function getCaptchaSecret(): ?string;
-}
