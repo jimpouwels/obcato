d212a78caea06553c1b211ff6ddb33332f04f1c9
Jim Pouwels <jim.pouwels@gmail.com>
2024-02-21 12:03:00 +0100

    update

--- src/admin/modules/authorization/visuals/UserEditor.php
+++ src/admin/modules/authorization/visuals/UserEditor.php
@@ -6,7 +6,7 @@ use Obcato\ComponentApi\TemplateData;
 use Obcato\Core\admin\modules\authorization\model\User;
 use Obcato\Core\admin\view\views\Panel;
 use Obcato\Core\admin\view\views\PasswordField;
-use Obcato\Core\admin\view\views\TextField;
+use Obcato\Core\admin\view\views\TextField;gi
 
 class UserEditor extends Panel {
 
