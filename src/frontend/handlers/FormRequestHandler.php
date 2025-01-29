3bbdd8c86d4f0dde7d2a0c84071e3d997bbd5f3e
Jim Pouwels <jim.pouwels@philips.com>
2025-01-29 08:57:55 +0100

    Change: Correctly verifying captcha

--- src/frontend/handlers/FormRequestHandler.php
+++ src/frontend/handlers/FormRequestHandler.php
@@ -80,11 +80,17 @@ class FormRequestHandler {
         $captchaToken = $_POST["captcha_token"];
         $secretKey = $this->configDao->getCaptchaSecret();
         $ip = $_SERVER['REMOTE_ADDR'];
-        $url = "https://www.google.com/recaptcha/api/siteverify?secret={$secretKey}&response={$captchaToken}&remoteip={$ip}";
+        $url = "https://www.google.com/recaptcha/api/siteverify";
+        $data = ['secret' => $secretKey, 'response' => $captchaToken, 'ip' => $ip];
+        $options = [
+            'http' => [
+                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method' => 'POST',
+                'content' => http_build_query($data),
+            ],
+        ];
 
-        $response = file_get_contents($url);
-        $jsonResponse = json_decode($response);
-
-        return $jsonResponse->success;
+        $context = stream_context_create($options);
+        return file_get_contents($url, false, $context);
     }
 }
