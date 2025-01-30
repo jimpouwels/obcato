f6228a5105410a17687284ee87189b86dd9f46e9
Jim Pouwels <jim.pouwels@philips.com>
2025-01-30 19:52:34 +0100

    Change: Add comment

--- src/frontend/PhotoAlbumElementFrontendVisual.php
+++ src/frontend/PhotoAlbumElementFrontendVisual.php
@@ -30,7 +30,7 @@ class PhotoAlbumElementFrontendVisual extends ElementFrontendVisual {
             $imageItem["alt_text"] = $image->getAltText();
             $imageItem["location"] = $image->getLocation();
             $imageItem["url"] = $this->getImageUrl($image);
-            list($width, $height) = getimagesize(UPLOAD_DIR . '/' . $image->getFilename());
+            list($width, $height) = getimagesize(UPLOAD_DIR . '/' . $image->getFilename()); // better have a getWidth() and getHeight() on the image?
             $imageItem["width"] = $width;
             $imageItem["height"] = $height;
             $images_arr[] = $imageItem;
