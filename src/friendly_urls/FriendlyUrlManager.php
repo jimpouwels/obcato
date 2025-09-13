5ea7eb28f425a39ddc5f12474b451127506be0cc
Jim Pouwels <jim.pouwels@gmail.com>
2025-09-13 11:22:47 +0200

    friendly url http access fix

--- src/friendly_urls/FriendlyUrlManager.php
+++ src/friendly_urls/FriendlyUrlManager.php
@@ -174,7 +174,6 @@ class FriendlyUrlManager {
             "RewriteCond %{REQUEST_URI} !^/index.php\n" .
             "RewriteRule ^sitemap.xml$ /index.php?sitemap=true [NC,L]\n" .
             "RewriteRule ^robots.txt$ /index.php?robots=true [NC,L]\n\n" .
-            "RewriteCond %{REQUEST_URI} !^/index(.*)\n" .
             "RewriteCond %{REQUEST_URI} !^/admin(.*)\n" .
             "RewriteRule ^.*$ index.php [NC,L]");
     }
